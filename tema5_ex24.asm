37da8ca42915c66635213ed5626cb2fe3464d840
Ioana-Valeria Gavrila <ioana.gavrila@stud.ubbcluj.ro>
2024-11-10 15:27:29 +0200

    Add files via upload
    new

---
+++ tema5_ex24.asm
@@ -0,0 +1,57 @@
+bits 32 ; assembling for the 32 bits architecture
+
+;se dau 2 siruri de octeti A si B. Sa se construiasca sirul R care sa contina elementele lui B in ordine inversa urmate de elementele in ordine inversa ale lui A.
+;Exemplu:
+;A: 2, 1, -3, 0
+;B: 4, 5, 7, 6, 2, 1
+;R: 1, 2, 6, 7, 5, 4, 0, -3, 1, 2
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ; ...
+    a db 2, 1, -3, 0
+    lungime equ $-a
+    b db 4, 5, 7, 6, 2, 1
+    l equ ($-b)
+    r times lungime+l db 0
+    
+
+; our code starts here
+segment code use32 class=code
+    start:
+        mov ecx, l
+        mov esi, l
+        mov edi, 0
+        dec esi
+        jecxz Sfarsit ;aici se verifica doar daca continutul din ecx e 0
+        Repeta:
+            mov al, [b+esi]
+            
+            mov[r+edi], al ;punem la pozitia corespunzatoare din d rezultatul
+            inc edi
+            dec esi
+        loop Repeta
+        Sfarsit:
+        mov ecx, lungime
+        mov esi, lungime
+        dec esi
+        jecxz SfarsitA ;aici se verifica doar daca continutul din ecx e 0
+        RepetaA:
+            mov al, [a+esi]
+            mov[r+edi], al ;punem la pozitia corespunzatoare din d rezultatul
+            inc edi 
+            dec esi
+        loop RepetaA
+        SfarsitA:
+        
+        
+    
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
---
+++ tema5_ex9.asm
@@ -0,0 +1,40 @@
+bits 32 ; assembling for the 32 bits architecture
+
+;Se da un sir de octeti S de lungime l. Sa se construiasca sirul D de lungime l-1 astfel incat elementele din D sa reprezinte diferenta dintre fiecare 2 elemente consecutive din S.
+;Exemplu:
+;S: 1, 2, 4, 6, 10, 20, 25
+;D: 1, 2, 2, 4, 10, 5
+global start        
+
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    ; ...
+    s db 1, 2, 4, 6, 10, 20, 25 ; stocare sir s
+    l equ $-s ; lungimea lui s
+    d times l-1 db 0 ;noul sir d
+
+; our code starts here
+segment code use32 class=code
+    start:
+        ; ...
+        mov ecx, l ;contor pt loop
+        mov esi, 0
+        jecxz Sfarsit ;aici se verifica doar daca continutul din ecx e 0
+        Repeta:
+            mov al, [s+esi]
+            inc esi
+            mov bl, [s+esi]
+            sub bl, al
+            
+            mov[d+esi+1], bl ;punem la pozitia corespunzatoare din d rezultatul
+            
+        loop Repeta
+        Sfarsit:
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
